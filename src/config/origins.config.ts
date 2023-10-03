82fdb343f6a4ca584a93aed39e637ccc62604d74
Prince Codes <princecodes247@gmail.com>
2023-10-04 00:43:23 +0100

    add thévercel

--- src/config/origins.config.ts
+++ src/config/origins.config.ts
@@ -3,6 +3,7 @@ const allowedOrigins: string[] = [
   'http://127.0.0.1:3000',
   'http://localhost:3000',
   'http://localhost:3001',
+  'https://veldora.vercel.com',
 ];
 
 export default allowedOrigins;
